925e74d9f90f0ce7c1c244f1e798b605362c07f6
Aayush7105 <aayushrawat5107@gmail.com>
2025-08-22 18:53:24 +0530

    assignment1 complete

---
+++ Assignments/Assignment1/que2.java
@@ -0,0 +1,20 @@
+// Sam’s mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100. Find the average percent mark in PCM
+// I/P => NONE
+// O/P => Sam’s average mark in PCM is ___ 
+
+
+public class que2{
+    public class SamAverage {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        int totalSubjects = 3;
+        double average = (maths + physics + chemistry) / (double) totalSubjects;
+
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
+
+}
---
+++ Assignments/Assignment1/que9.java
@@ -0,0 +1,24 @@
+// Write a new program similar to the program # 6 but take user input for Student Fee and University Discount
+// Hint => 
+// Create a variable named fee and take user input for fee.
+// Create another variable discountPercent and take user input.
+// Compute the discount and assign it to the discount variable.
+// Compute and print the fee you have to pay by subtracting the discount from the fee.
+
+
+import java.util.Scanner;
+
+public class que9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the course fee: ");
+        double fee = input.nextDouble();
+        System.out.print("Enter the discount percentage: ");
+        double discountPercent = input.nextDouble();
+
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+    }
+}
