a6e295b640040046b046fede818124422b34f59c
Aayush7105 <aayushrawat5107@gmail.com>
2025-09-14 21:56:11 +0530

    Done with oops assignment of inbuilt functions

---
+++ Practice/Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+    public void speak() {
+        System.out.println("Animal makes a sound");
+    }
+    public static class Dog extends Animal {
+        @Override
+        public void speak() {
+            System.out.println("Dog barks");
+        }
+    }
+    public static class Cat extends Animal {
+        @Override
+        public void speak() {
+            System.out.println("Cat meows");
+        }
+    }
+    public static void main(String[] args){
+        Animal a = new Animal();
+        Animal d = new Dog();
+        Animal c = new Cat();   
+        d.speak(); 
+        c.speak(); 
+        a.speak();
+    }
+}
